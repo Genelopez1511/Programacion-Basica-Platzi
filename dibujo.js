efea50c9ed8fa1147c6fed4c8477f178e3d90dbe
Genelopez1511 <80583722+Genelopez1511@users.noreply.github.com>
2021-05-01 11:00:52 -0500

    Canvas

---
+++ dibujo.js
@@ -0,0 +1,38 @@
+  var texto = document.getElementById ("texto_lineas");
+  var boton = document.getElementById ("botoncito");
+  boton.addEventListener("click", dibujoPorClick);
+
+  var d = document.getElementById ("dibujito");
+  var ancho = d.width;
+  var lienzo = d.getContext("2d");
+
+  function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+  {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+  }
+  function dibujoPorClick()
+  {
+  var xxx = parseInt(texto.value);
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var y1, xf;
+  var colorcito = "#FAA";
+  var espacio = ancho / lineas;
+
+  for(l = 0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    dibujarLinea("purple", 300, yi, xf, 0);
+    console.log("lineas" + 1);
+  }
+
+  dibujarLinea (colorcito, 1,1,1,299);
+  dibujarLinea(colorcito, 1,299,299,299);
+  }
